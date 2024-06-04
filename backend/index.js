7b38396967c0c7f02cc16ddc193f772c7c1e16a5
Abhijith Goud Kancharla <abhijith1024283@gmail.com>
2024-06-05 00:50:41 +0530

    intialisation

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+
+app.get('/', (req,res) =>{
+    res.send('Welcome to the Backend Server');
+})
+
+
+app.listen(port, (req,res) =>{
+    console.log(`Server started on port ${port}`);
+})
