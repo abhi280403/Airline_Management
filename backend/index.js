dba7372523882dc116e96836f01c60ea49f9897b
Abhijith Goud Kancharla <abhijith1024283@gmail.com>
2024-06-11 20:53:09 +0530

    database setup

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,37 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRouter from "./routes/auth.js";
+
+
 const app = express();
-
-const port = 4840;
-
-
-app.get('/', (req,res) =>{
+const port = 4841;
+dotenv.config();
+app.use(express.json());
+app.get('/', (req, res) => {
     res.send('Welcome to the Backend Server');
-})
+});
+
+// Encode password special characters
+const password = encodeURIComponent('Abhijith@12345');
+
+// Correct MongoDB connection string format
+const uri = `mongodb+srv://abhijithgoud757:${password}@airline.hjeq4bt.mongodb.net/?retryWrites=true&w=majority&appName=Airline`;
 
 
-app.listen(port, (req,res) =>{
+const connectDB = async () => {
+    try {
+      await mongoose.connect(uri);
+      console.log(`MongoDB connected`);
+    } catch (error) {
+      console.error("MongoDB connection error:", error);
+    }
+  };
+
+
+app.use('/api', authRouter);
+app.listen(port, () => {
+    connectDB();
     console.log(`Server started on port ${port}`);
-})
+});
+
